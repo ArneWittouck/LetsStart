1269b1dcc958343a16a0adde658ce960abb82fc7
Arne Wittouck <arne.wittouck@student.vives.be>
2023-09-19 11:51:52 +0200

    Commit message Add counter

--- src/main/java/be/vives/ti/StringProcessor.java
+++ src/main/java/be/vives/ti/StringProcessor.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 public class StringProcessor {
     public String appendIfMissing (String str, String suffix) {
         int i = 0;
+
         return StringUtils.appendIfMissing(str, suffix);
     }
 
